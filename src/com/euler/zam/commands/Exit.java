e89c9ea34627810d53576c546feaecfe4fb47ffe
Cody Wofford <cody.e.wofford@gmail.com>
2019-08-21 14:26:26 -0400

    Exit command.

---
+++ src/com/euler/zam/commands/Exit.java
@@ -0,0 +1,13 @@
+package com.euler.zam.commands;
+
+import com.euler.zam.Command;
+
+/**
+ * Exit Command
+ */
+public class Exit implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
